5b78cf71e5785be3de859cbed574412b3d462448
hhss2259 <hhss2259@naver.com>
2023-08-10 17:14:56 +0900

    deltet yml

---
+++ src/test/java/kpop/kpopGeneration/security/oauth2/CustomOauth2UserServiceTest.java
@@ -0,0 +1,10 @@
+package kpop.kpopGeneration.security.oauth2;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CustomOauth2UserServiceTest {
+
+}
